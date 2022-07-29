c6739fff7e5d6ee16fcd5eeafb1b873c7689485e
Alexander Galichev <lex.galichev@gmail.com>
2022-07-29 15:50:53 +1000

    SF.Module9 - Task 1 Editing as recommended

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,13 +1,8 @@
-﻿Exception myExeption = new Exception("Пользовательское исключение");
-myExeption.HelpLink = "https://docs.microsoft.com/ru-ru/dotnet/csharp/fundamentals/exceptions/";
-myExeption.Data.Add("Дата создания исключения: ", DateTime.Now);
-
-Exception[] exceptions = new Exception[5];
-exceptions[0] = myExeption;
-exceptions[1] = new ArgumentException("Недопустимый аргумент");
-exceptions[2] = new FormatException("Значение не соответствует формату преобразования");
-exceptions[3] = new FileNotFoundException("Такой файл не существует");
-exceptions[4] = new OverflowException("Операция привела к переполнению");
+﻿List<Exception> exceptions = new List<Exception>() {new MyException("Пользовательское исключение"),
+                                                     new ArgumentException("Недопустимый аргумент"),
+                                                     new FormatException("Значение не соответствует формату преобразования"),
+                                                     new FileNotFoundException("Такой файл не существует"),
+                                                     new OverflowException("Операция привела к переполнению")};
 
 foreach (Exception e in exceptions)
 {
@@ -15,14 +10,13 @@ foreach (Exception e in exceptions)
     {
         throw e;
     }
+    catch (MyException ex)
+    {
+        Console.WriteLine($"{ex.Message}\nПерейдите по ссылке https://docs.microsoft.com/ru-ru/dotnet/csharp/fundamentals/exceptions/");
+    }
     catch (Exception ex)
     {
-        if (ex.Equals(myExeption)) 
-        {
-            Console.WriteLine($"{myExeption.Message}\nПерейдите по ссылке {ex.HelpLink}");
-
-        } 
-        else Console.WriteLine($"Ошибка: {ex.Message}");
+        Console.WriteLine($"Ошибка: {ex.Message}");
     }
     finally
     {
@@ -31,3 +25,8 @@ foreach (Exception e in exceptions)
 }
 
 Console.ReadLine();
+
+public class MyException : Exception
+{
+    public MyException(string message) : base(message) { }
+}
